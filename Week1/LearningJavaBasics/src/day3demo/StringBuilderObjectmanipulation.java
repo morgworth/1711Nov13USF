424f313341f58e0d930a059a6fa024b6fe8780b7
Rhodey35 <4arhodes@gmail.com>
2017-11-15 19:58:40 -0500

    This is day three commit of training and coding Nov 15

---
+++ Week1/LearningJavaBasics/src/day3demo/StringBuilderObjectmanipulation.java
@@ -0,0 +1,18 @@
+package day3demo;
+/*
+ * Finish this tonight: 
+ * Example of using codepoint
+ * https://www.tutorialspoint.com/java/lang/stringbuilder_codepointat.htm
+ * and 
+ * reference oracle at 
+ * https://docs.oracle.com/javase/7/docs/api/java/lang/StringBuilder.html
+ * 
+ */
+public class StringBuilderObjectmanipulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder useritem = new StringBuilder("This is a test string");
+	}
+
+}
--- Week1/MyFirstJavaProjectAlive/src/com/ex/animals/Human.java
+++ Week1/MyFirstJavaProjectAlive/src/com/ex/animals/Human.java
@@ -2,6 +2,33 @@ package com.ex.animals;
 
 public class Human extends Animal{
 
+	
+	private int age;
+	private String haircolor;
+	private double height;
+	
+	public Human(){}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getHaircolor() {
+		return haircolor;
+	
+	}
+	
+	public void setHaircolor(String haircolor) {
+		this.haircolor = haircolor;
+	}
+	
+	
+	
+	
 	public int reproduce(int numPartiesInvolved) {
 		// TODO Auto-generated method stub
 		return 0;
